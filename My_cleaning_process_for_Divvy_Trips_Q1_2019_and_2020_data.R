05f26bdd23954315a7cd5f2e6af774124db23271
The_Analyst <udohchioma78@gmail.com>
2026-02-26 11:50:02 +0100

    Update My_cleaning_process_for_Divvy_Trips_Q1_2019_and_2020_data.R

--- My_cleaning_process_for_Divvy_Trips_Q1_2019_and_2020_data.R
+++ My_cleaning_process_for_Divvy_Trips_Q1_2019_and_2020_data.R
@@ -30,7 +30,7 @@
   View(clean2_Divvy_Trips_2019_Q1)
   
   
-  # To detect inconsistent suffixes using the full name of observed abreviations prominent in the dataframe
+  # To detect inconsistent suffixes using the full name of observed abbreviations prominent in the dataframe
   patterns <- c("\\bRoad\\b", "\\bStreet\\b", "\\bParkway\\b", "\\bAvenue\\b",
                 "\\bBoulevard\\b", "\\bPlace\\b", "\\bDrive\\b")
   
@@ -301,16 +301,16 @@
             "clean5_Divvy_Trips_2020_Q1.csv",
             row.names = FALSE)
   
-  write.csv(clean6_Divvy_Trips_2020_data,
-            "clean6_Divvy_Trips_2020_data.csv",
+  write.csv(clean6_Divvy_Trips_2020_Q1,
+            "clean6_Divvy_Trips_2020_Q1.csv",
             row.names = FALSE)
   
-  write.csv(clean7_Divvy_Trips_2020_data,
-            "clean7_Divvy_Trips_2020_data.csv",
+  write.csv(clean7_Divvy_Trips_2020_Q1,
+            "clean7_Divvy_Trips_2020_Q1.csv",
             row.names = FALSE)
   
-  write.csv(clean8_Divvy_Trips_2020_data,
-            "clean8_Divvy_Trips_2020_data.csv",
+  write.csv(clean8_Divvy_Trips_2020_Q1,
+            "clean8_Divvy_Trips_2020_Q1.csv",
             row.names = FALSE)
   
   
@@ -355,9 +355,9 @@
   
   
   #I arranged rows in descending order to check for false tripduration_seconds 
-  #and spoted some -ve values and some vakues less thn 60s which i use as my bech benchmark for rides
-  #I also noticed some data with aoutrageous amounts of time spent on rides when u arranged by clicking on the 
-  #arro in the r sorce(aspa that place that shows the satasets) and so i set a benc hmark to emove all greater than 3 hrs tha is 10800seconds
+  #and spotted some -ve values and some values less than 60s which I use as my benchmark for rides
+  #I also noticed some data with outrageous amounts of time spent on rides when u arranged by clicking on the 
+  #arrow in the r source(that place that shows the datasets) and so I set a benchmark to remove all hours greater than 3 hrs that is 10800seconds
   
   Original_merged_rows <- nrow(merged_divvy_data4)
   View(Original_merged_rows)
@@ -386,7 +386,7 @@
   View(merged_divvy_data6)
   
   
-  #ANALYSES PHASE (I GUESS...LOL)
+  #ANALYSES PHASE
   
   #mean and median trip duration for each usertyes
   summary_tripduration <- merged_divvy_data6 %>%
@@ -404,16 +404,16 @@
            median_tripduration_mins = median_tripduration_secs / 60)%>%
     select(-avg_tripduration_secs, -median_tripduration_secs)
   
-  #The avg_trip duration for casual users was 31minutes and from member was 11 min
+  #The avg_trip duration for casual users was 31 minutes and from member was 11 minutes
   #This was closely related to the median values of 22.3 minutes for casual users and 
   #8.48 minutes for members.
   #This implies that casual riders use bike for a longer duration than members
   
   
-  #We now dive deeper to observe how these two users differs by days of the week
-  #I realised i still had two columns and decided to work with a sinlge colium for dow but then 
-  #going through my data i realised that some hrs crossed over from one day into another and so
-  #I picked the start day as my day of week and proceeded to exploring the differement bewteen users by dow
+  #We now dive deeper to observe how these two users differs by days of the week(dow)
+  #I realised I still had two columns and decided to work with a sinlge column for dow but then 
+  #going through my data I realised that some hrs crossed over from one day into another and so
+  #I picked the start day as my day of week and proceeded to exploring the differment bewteen users by dow
   
   
   all(merged_divvy_data6$start_day_of_the_week ==
@@ -470,11 +470,11 @@
   #with peaks on weekends. Members show stable, shorter ride durations during weekdays,
   #reinforcing commuter behavior.
   #The highest average trip duration for both casual and members were on sundays. 
-  #For both groups, the mean is greater than the median showing that tthe datsa is  slighlty skwed.
+  #For both groups, the mean is greater than the median showing that the data is slighlty skewed.
   #Casual users consistently took longer trips than members, with average trip durations 
   #peaking on Sundays. Median values show a similar pattern, 
   #confirming that casual users’ rides are both longer and slightly more variable.
-  #for Lets see if this will 
+  #Lets see if this will 
   #be same for the count per day to see which day bike is being used the most
  
   
@@ -584,18 +584,18 @@
       fill = "User Type"
     ) +
     theme_minimal()
- #Casual riders use bikes the mostly on weekends with the amount of times bokes 
-  #where order on satand subn as 15507 and 8582 times.There was a huge drop on weekdays 
-  #Members use bikes at a high count rate on weeddays, even about 7 times more than the caual iridders use it on weekends
+ #Casual riders use bikes mostly on weekends with the amount of times bikes 
+  #where ordered on saturday and sunday as 15507 and 8582 times. There was a huge drop on weekdays 
+  #Members use bikes at a high count rate on weekdays, even about 7 times more than the casual riders use it on weekends
   #On average no of bikes picked up by members on weeekdays are 119046 bikes and 59059 on weeekends
-  #While avg  count of bikes casual users is  12044 on weekend and , 5047 on weeekdays
-  #we observed that bikes are used mostly bike members tha by casual riders. 
-  #While long trips are made by casual users they dont use bikes as much as often as the members
-  #who use it very often but take shorter trip in term of trip length
-  
-  
-  #We now dive deeper  to see which hours for each users , which hour  users chose to ride
-  #i.e which is the most recorded hrs for the start_time for each users
+  #While avg count of bikes casual users is  12044 on weekend and, 5047 on weeekdays
+  #we observed that bikes are used mostly by members than by casual riders. 
+  #While long trips are made by casual users they don't use bikes as often as the members
+  #who use it very often but take shorter trip in terms of trip length
+
+
+  #We now dive deeper to see which hour users choose to ride by usertype
+  #i.e which is the most recorded hour for the start_time for each users
   
   merged_divvy_data9 <- merged_divvy_data8 %>%
     mutate(start_hour = hour(start_time)) %>%
@@ -611,9 +611,9 @@
   View(trips_by_hour_overall)
   
   
-  #Overall , most trips occurs between 7am - 8 am and 4pm -5pm . The east occrs a 3ami predict that the
-  #members might be skewing this dataset in terms of when most rides ocur so we dive deper to seeing if this is so for each user
-  #To see if theres a  difference in hourly behaviour 
+  #Overall, most trips occurs between 7am - 8 am and 4pm - 5pm. The least occurs at 3am. I predict that the
+  #dataset might be skewed by members, in terms of when most rides occur so we dive deeper to see if this is so for each user
+  #To see if there's a difference in hourly behaviour 
   
   
   #trip Count by usertype and start hour o
@@ -623,15 +623,15 @@
     summarise(trip_count = n(), .groups = "drop") %>%
     arrange(usertype, desc(trip_count))
   View(hourly_trip_by_usertype)
-  #Casual users peak around 13–17 (1 PM–5 PM) → afternoon/early evening
-  #casual riders raraely ride at 0-5(12am-5am) that is at night / very early morning
-  #Members peak around 7–8am and again around 4-6pm → mostly daytime commuter hours
-  #late morning 9am and midday rides 12-3 exists but are not as intense at commuting hours
-  #members similarly to cusal rideers ride least at night from 0-4 am with a slighly higher amunt from 
+  #Casual users peak around 13–17 (1pm - 5pm), that is, afternoon/early evening
+  #casual riders rarely ride at 0-5(12am - 5am), that is, at night / very early morning
+  #Members peak around 7am - 8am and again around 4pm - 6pm, that is,  mostly daytime commuter hours
+  #late morning 9am and midday rides 12am - 3am exists but are not as intense as commuting hours
+  #members similarly to cusal rideers ride least at night from 12am - 4am with a slighly higher amunt from 
   
   
   
-  #we now see if theres stations that are popular for members and for casual riders
+  #we now see if there are stations that are popular for members and for casual riders
   # Top start stations overall
   top_start_stations <- merged_divvy_data9 %>%
     group_by(start_station_name) %>%        # Group by start station
@@ -647,7 +647,7 @@
     arrange(usertype, desc(station_count)) 
   View(top_start_stations_usertype)
   
-  #we now see if theres stations that are popular for members and for casual riders
+  #we now see if there are stations that are popular for members and for casual riders
   # Top start stations overall
   top_end_stations <- merged_divvy_data9 %>%
     group_by(end_station_name) %>%        # Group by start station
@@ -746,5 +746,6 @@
             row.names = FALSE)
   
   
+
 
   
